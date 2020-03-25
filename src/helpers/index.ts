a3de8e11b8b0f3c6550b091d53ac0833c2edd8f6
gil <gil.bueno@simpli.com.br>
2020-03-25 14:51:10 -0300

    removing unused/commented imports

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,6 @@
-// export * from './file'
 import * as filter from './filter'
-// export * from './resource'
 import * as router from './router'
 import * as toast from './toast'
-// import * from './transform'
 import * as various from './various'
 import * as downloadCsv from './downloadCsv'
 
